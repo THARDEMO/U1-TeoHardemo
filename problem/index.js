aa26eada3d9af9dde0693e8271d3ba1f30a3eb10
Teo Hardemo <teo.hardemo@gmail.com>
2023-02-15 11:01:25 +0100

    General CSS
    added the general css for the projekt, different files for accounts and questions,

---
+++ problem/index.js
@@ -0,0 +1,27 @@
+// "use strict";
+
+// var input = document.querySelector('input'); // get the input element
+// input.addEventListener('click', resizeInput); // bind the "resizeInput" callback on "input" event
+// // resizeInput.call(input); // immediately call the function
+
+// function resizeInput( event) {
+//   this.target.value = this.target.length + "1ch"
+// }
+
+/*      
+    LOGIN / REGISTER screen
+
+        <div>LOGIN / Register</div>
+        <div id="inputFields">
+            <label for="username">Username:</label>
+            <input type="text" name="username">
+            
+            <label for="password">Password:</label>
+            <input type="text" name="password"> 
+        </div>
+
+        <div>Let the magic start!</div>
+        <button>Login</button>
+        <div><a href="#">New to this? Register for free</a></div> 
+        
+*/
